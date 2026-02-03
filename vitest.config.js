3ae130172c6e09225f9930d76aed599495f38964
Allister Antosik <me@allisterantosik.com>
2026-02-03 16:00:10 +0000

    Add worker tests and CI

---
+++ vitest.config.js
@@ -0,0 +1,12 @@
+import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
+
+export default defineWorkersConfig({
+  test: {
+    include: ["tests/workers/**/*.test.js"],
+    poolOptions: {
+      workers: {
+        wrangler: { configPath: "./wrangler.toml" },
+      },
+    },
+  },
+});
