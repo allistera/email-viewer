20928c4e7f913ba8db6be9ccdabd940e3bd62daf
Allister Antosik <me@allisterantosik.com>
2026-03-10 12:03:50 +0000

    UI: Always show spam count in sidebar

--- web/src/components/DiscordSidebar.vue
+++ web/src/components/DiscordSidebar.vue
@@ -81,7 +81,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
               </svg>
               <span class="nav-label">Spam</span>
-              <span v-if="spamCount" class="nav-badge danger">{{ spamCount }}</span>
+              <span class="nav-badge danger">{{ spamCount }}</span>
             </button>
           </li>
         </ul>
